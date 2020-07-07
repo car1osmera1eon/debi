51fbae0022c70260fc4f3e61b2aff9d2eecce1b6
carlosmeraleon <carlosmeraleon@gmail.com>
2020-07-07 17:59:25 -0500

    actualizacion

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use Session;
 // use App\Models\perfiles\Sistema;
 // use App\Models\Usuario;
@@ -32,7 +32,7 @@ class SitioController extends Controller
      */
     public function index()
     {
-        
+
     }
 
     public function agenda(Request $request)
@@ -44,7 +44,7 @@ class SitioController extends Controller
         $medicos            = \App\Models\maestros\M_medico::get();
         $allprocedimientos  = \App\Models\maestros\M_procedimiento::limit(6)->get();
         $agendaproced       = \App\Models\maestros\M_procedimiento::where('agenda_sitio','1')->get();
-        if(isset($request->medico_id)){ 
+        if(isset($request->medico_id)){
             $medico         = \App\Models\maestros\M_medico::where('id', $request->medico_id)->first();
             $medico_id      = $request->medico_id;
             $presentacion   = $request->presentacion;
@@ -60,18 +60,18 @@ class SitioController extends Controller
     public function getTodoAgenda()
     {
         $data       = array();    $evento = array();
-        $agenda     = $this->agendaRepository->all(); 
-        
+        $agenda     = $this->agendaRepository->all();
+
         foreach($agenda as $row){
-            $evento['id']     = $row->id;             
-            $evento['title']  = "No Disponible";     
-            $evento['start']  = date('Y-m-d',strtotime($row->fechaini)).' '.$row->horaini;       
-            $evento['end']    = date('Y-m-d',strtotime($row->fechafin)).' '.$row->horafin;  
-            // $evento['url']    = route('agendas.edit',[$row->id]);  
-            
-            $data[] = $evento;    
+            $evento['id']     = $row->id;
+            $evento['title']  = "No Disponible";
+            $evento['start']  = date('Y-m-d',strtotime($row->fechaini)).' '.$row->horaini;
+            $evento['end']    = date('Y-m-d',strtotime($row->fechafin)).' '.$row->horafin;
+            // $evento['url']    = route('agendas.edit',[$row->id]);
+
+            $data[] = $evento;
         }
-        return response()->json($data); 
+        return response()->json($data);
     }
 
     public function consultaAgenda(Request $request){
@@ -86,18 +86,18 @@ class SitioController extends Controller
         if(isset($request->fechadesde)){
             $desde          = $request->fechadesde;
         }else{
-            $desde          = date('Y-m-d'); 
+            $desde          = date('Y-m-d');
         }
         if(isset($request->fechahasta)){
             $hasta          = $request->fechahasta;
         }else{
-            $hasta          = date('Y-m-d'); 
+            $hasta          = date('Y-m-d');
         }
-        if(isset($request->profesional_id)){ 
+        if(isset($request->profesional_id)){
             $medico         = \App\Models\maestros\M_medico::where('id', $request->profesional_id)->first();
             $medico_id      = $request->profesional_id;
             $presentacion   = $request->presentacion;
-            
+
             $bussinesHour   = Agenda::horarioLaboralMedico($request->medico_id);
         }else{
             $medico         = \App\Models\maestros\M_medico::find(1);
@@ -105,22 +105,37 @@ class SitioController extends Controller
             $bussinesHour   = Agenda::horarioLaboralMedico(1);
         }
 
+        $desde              =   str_replace('/', '-', $desde);
+        $timestamp          =   \Carbon\Carbon::parse($desde)->timestamp;
+        $desde              =   date('Y-m-d', $timestamp);
+
+        $hasta              =   str_replace('/', '-', $hasta);
+        $timestamp          =   \Carbon\Carbon::parse($hasta)->timestamp;
+        $hasta              =   date('Y-m-d', $timestamp);
+
+        $desde              =   strtotime($desde);
+        $hasta              =   strtotime($hasta);
+
+        Session::put('agendadesde', $desde);
+        Session::put('agendahasta', $hasta);
+
         $semana = $this->crearSemana($request->profesional_id,$desde,$hasta,$empresa);
 
         return view('sitio.resultadoconsulta', compact('empresa','sitio','allprocedimientos','medicos','presentacion','bussinesHour','medico_id','desde','hasta','semana','medico'));
-        
+
     }
 
     public function crearSemana($medico_id,$fechadesde,$fechahasta,$empresa)
     {
         // dd($tiempo_atencion);
         $nom_dias               =   array('Dom','Lun','Mar','Mie','Jue','Vie','Sab');
-        $desde                  =   $fechadesde;
-        $fechadesde             =   str_replace('/', '-', $fechadesde);
-        $timestamp              =   \Carbon\Carbon::parse($fechadesde)->timestamp;
-        $desde                  =   date('Y-m-d', $timestamp);
+        // $desde                  =   $fechadesde;
+        // $fechadesde             =   str_replace('/', '-', $fechadesde);
+        // $timestamp              =   \Carbon\Carbon::parse($fechadesde)->timestamp;
+        $desde                  =   date('Y-m-d', $fechadesde);
+        // dd($desde);
         $ndia                   =   date('N', strtotime($desde));
-        
+
         list($anio, $mes, $dia) =   explode("-", $desde);
 
         $nuevaFecha             =   mktime(0,0,0,$mes,$dia,$anio); // m/d/y
@@ -141,7 +156,7 @@ class SitioController extends Controller
         // dd($inicio_atencion);
         $semana     .="</tr></thead><tbody>";
         // $horario    =   $this->getHorarioLaboralxId($medico_id);
-        // dd($horario);
+        // dd(date('Y-m-d',Session::get('agendahasta')));
         $inicio_atencion                =   $empresa->inicio_atencion;
         while(strtotime($empresa->fin_atencion) > strtotime($inicio_atencion))
         {
@@ -152,12 +167,14 @@ class SitioController extends Controller
             for($i=0;$i<7;$i++){
                     // TODO FECHA DEL DIA
                     $fecha_dia          =   date ("Y-m-d",$nuevaFecha+$i*24*3600);
-                    // TODO CONSULTAR LOS HORARIOS LABORABLES
-                    if(!$this->getHorarioLaboralxId($medico_id,($i),1,$inicio_atencion_ant)){
-                        $semana         .= "<td style='background-color:#d1cece' > <span class=\"label label-primary\">&nbsp;</span> </td>";   
+                    // echo date('Y-m-d',Session::get('agendahasta'))."::".($fecha_dia)."<br>";
+                    if(Session::get('agendahasta') < strtotime($fecha_dia) or Session::get('agendadesde') > strtotime($fecha_dia) ){
+                        $semana         .= "<td style='background-color:#d1cece' > <span class=\"label label-primary\">&nbsp;</span> </td>";
+                    }elseif(!$this->getHorarioLaboralxId($medico_id,($i),1,$inicio_atencion_ant)){// TODO CONSULTAR LOS HORARIOS LABORABLES
+                        $semana         .= "<td style='background-color:#d1cece' > <span class=\"label label-primary\">&nbsp;</span> </td>";
                     }elseif($this->getAgendaOcupado($medico_id,$fecha_dia,$inicio_atencion_ant,$inicio_atencion)){
-                        $semana         .= "<td> <button class=\"btn btn-danger\" disabled><i class=\"fa fa-calendar-times-o\"></i> OCUPADO </button> </td>";  
-                        // $semana         .= "<td style='background-color:#ff9696' > <span class=\"label label-primary\">OCUPADO</span> </td>";  
+                        $semana         .= "<td> <button class=\"btn btn-danger\" disabled><i class=\"fa fa-calendar-times-o\"></i> OCUPADO </button> </td>";
+                        // $semana         .= "<td style='background-color:#ff9696' > <span class=\"label label-primary\">OCUPADO</span> </td>";
                     }else{
                         $semana         .= "<td > <button class=\"btn btn-primary\"><i class=\"fa fa-calendar-check-o\"></i> AGENDAR </button></td>";
                     }
@@ -178,14 +195,14 @@ class SitioController extends Controller
 			}
 		return $horaSplit;
     }
-    
+
     function SumaHoras( $time1, $time2 ){
         // dd($time1);
-		list($hour1, $min1, $sec1) = $this->parteHora($time1); 
+		list($hour1, $min1, $sec1) = $this->parteHora($time1);
 		list($hour2, $min2, $sec2) = $this->parteHora($time2); //dd($this->parteHora($time2));
 		return date('H:i:s', mktime( $hour1 + $hour2, $min1 + $min2, $sec1 + $sec2));
     }
-    
+
     public function getHorarioLaboralxId($id,$ndia,$tipo,$hini)
     {
         # code...
@@ -219,5 +236,5 @@ class SitioController extends Controller
             return false;
         }
     }
-    
+
 }
